3774f17188572a4a191908a0beec60302ddb3c81
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-08-03 19:45:28 +0200

    add embed video

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import EmbedVideo from "./components/EmbedVideo";
 
-function App() {
+function App() {  
   return (
     <div className="App">
       <header className="App-header">
         <h1>Video Recommendation</h1>
+        <EmbedVideo />
       </header>
     </div>
   );
