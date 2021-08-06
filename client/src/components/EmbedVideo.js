09fa9ebfc13d851112ec0c7b9a4f80cb8eb184e8
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-08-06 12:34:24 +0200

    Update EmbedVideo.js

--- client/src/components/EmbedVideo.js
+++ client/src/components/EmbedVideo.js
@@ -1,56 +1,18 @@
 import React, { useState } from 'react';
-import {exampleresponse} from './../../../exampleresponse.json'
+import ExampleResponse from './../assets/exampleResponse.json';
+import AddVideo from './AddVideo';
+import Video from './Video';
+
+const data = ExampleResponse
+console.log(data)
 
 
-function EmbedVideo() {
-  const [votes, setVotes] = useState(0);
-  const [title, setTitle] = useState('');
-  const [url, setUrl] = useState('');
-  
-  const data = exampleresponse
-  console.log(data)
-  
-  // Delete a Video
-  const deleteVideo = () => {
-    console.log(`Add logic to delete video`);
-  };
-
-  // Add a Video
-  const addVideo = () => {
-    console.log(`Add logic for adding video`);
-  };
-
-  // Getting the Input Value
-  const handleInput = () => {
-    if (title.length !== 0 && url.length !== 0) {
-      console.log(`Got this: ${title}`);
-      console.log(`Got this: ${url}`);
-    }
-  }
-
-  // 
+function EmbedVideo() { 
 
   return (
     <div>
-      <h2>Title of video</h2>
-
-      <button onClick={() => setVotes(votes + 1)}>Up Vote</button>
-      <button onClick={() => setVotes(votes - 1)}>Down Vote</button>
-      <p>{votes}</p>
-      
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/VjXzWrgHSWk" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-      <button onClick={deleteVideo}>Delete</button>            
-      <br />
-      <br />
-      <label for='title' >Title</label>
-      <br />
-      <input value={title} onChange={(e)=>{setTitle(e.target.value)}} placeholder='enter title' type='text' id='title' name='title' />
-      <br />
-      <label for='title' >URL</label>
-      <br />
-      <input value={url} onChange={(e) => { setUrl(e.target.value) }}  placeholder='enter url' type='text' id='url' name='url' />
-      <br />
-      <button onClick={handleInput}>Add Video</button>
+      <Video />
+      <AddVideo />
     </div>
   )
 }
