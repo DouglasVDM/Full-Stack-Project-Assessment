8fbbcf55c156377df408b9df24d0a7a48b708ff7
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-08-08 15:32:43 +0200

    pass url to iframe for display
    -filtered the url from all the url's
    -trimmed the filtered url to only pass the id to the iframe embed url

--- client/src/components/Video.js
+++ client/src/components/Video.js
@@ -1,13 +1,22 @@
 import React, {useState} from 'react'
 import DeleteButton from './DeleteButton';
+import DownVoteButton from './DownVoteButton';
+import UpVoteButton from './UpVoteButton';
 
-function Video({ videoData }) {
-  console.log(videoData);
+function Video({ url, state }) {
+  const allVideos = state;
   const [votes, setVotes] = useState(0);
 
+  // Extracting the Video Id from the URL
+  const videoId = url.split('=').pop();
+
+  const filteredVideo = allVideos.filter(video => video.url === url);
+
   // Delete a Video
-  const deleteVideo = () => {
+  const deleteVideo = (url) => {
     console.log(`Add logic to delete video`);
+    allVideos.filter(video => video.url !== url);
+    
   };
 
   const upVote = (setVotes, votes) => {
@@ -20,26 +29,25 @@ function Video({ videoData }) {
   
   return (
     <div>
-      <h2>Title: .title</h2>
-      <h3>Rating: .rating</h3>
-      
-      <button onClick={upVote(setVotes, votes)}>
-        Up Vote
-      </button>
-      
-      <button onClick={downVote(setVotes, votes)}>
-        Down Vote
-      </button>
+      {filteredVideo.map(video => {
+        return (
+          <>
+            <h2>Title: {video.title}</h2>
+            <h3>Rating: {video.rating}</h3>
+            <UpVoteButton votes={votes} setVotes={setVotes} upVote={upVote} />
+            <DownVoteButton votes={votes} setVotes={setVotes} downVote={downVote} />      
+            <p>{votes}</p>
+          </>
+        )
+      })}
 
-      <p>{votes}</p>
-
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/VjXzWrgHSWk" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+      <iframe width="560" height="315" src={`https://www.youtube.com/embed/${videoId}`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
 
       <DeleteButton deleteVideo={deleteVideo} />      
     </div>
   )
-}
+};
 
-export default Video
+export default Video;
 
 
