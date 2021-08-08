fa18a62077bc62f63d5cc3f3d451cbef4f02841b
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-08-08 21:53:35 +0200

    saving input values to state variable
    Using map to show added videos on the screen.

--- client/src/components/AddVideo.js
+++ client/src/components/AddVideo.js
@@ -1,49 +1,90 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
+import validator from 'validator';
+import { uuid } from 'uuidv4';
 
 function AddVideo() {
+  const [errorMessage, setErrorMessage] = useState('');
   const [ title, setTitle] = useState('');
-  const [ url, setUrl] = useState('');
+  const [url, setUrl] = useState('');
+  const [videos, setVideos] = useState([]);
 
-  // Add a Video
-  const addVideo = () => {    
-  };
-  
-  // Getting the Input Value
-  const handleInput = () => {
-    if (title.length !== 0 && url.length !== 0) {
-      console.log(`Got this: ${title}`);
-      console.log(`Got this: ${url}`);
-      console.log(`Add logic for adding video`);
+  const validate = (value) => {
+    if (validator.isURL(value)) {
+      setErrorMessage('Is Valid URL')
+    } else {
+      setErrorMessage('Is Not Valid URL')
+    }
+  }
+
+  // Getting the Input Value, adding it to state
+  const handleSubmit = (event) => {
+    event.preventDefault();    
+    if (title && url) {
+      const video = {
+        id: uuid(),
+        title,
+        url,
+        rating: 0
+      };
+      setVideos((videos) => {
+        return [...videos, video];
+      });
+      setTitle('');
+      setUrl('');
+    } else {
+      console.log(`Enter title and url`);
     }
   }
 
   return (
-    <div>     
-        <label>
-          Title
-          <input
-            type="text"
-            name="title"
-            placeholder="Add your Video Title"
-            value={title}
-          onChange={(e) => setTitle(e.target.value)}
+    <div>
+      <form className="form" onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="title">Title:</label>          
+        <input
+          type="text"
+          id="title"
+          name="title"
+          value={title}          
+          placeholder="Add your Video Title"          
+          onChange={(event) => setTitle(event.target.value)}
           />
-      </label>
-      <br/>      
-        <label>
-          Url
-          <input
-            type="text"
-            name="title"
-            placeholder="Add the Video URL"
-            value={url}
-          onChange={(e) => setUrl(e.target.value)}
-          />
-      </label>
-      <br />      
-      <button onClick={handleInput}>Add Video</button>
+      </div>
+      <br />
+        <label htmlFor="url">URL: </label>
+        <input
+          type="text"
+          id="url"
+          name="url"
+          value={url}
+          placeholder="Add the Video URL"
+          onChange={(event) => {
+          validate(event.target.value)
+          setUrl(event.target.value)
+          }}>          
+        </input>
+        <br />
+        <span style={{ fontWeight: 'bold', color: 'red', }}>
+          {errorMessage}
+        </span>
+        <br />
+      <button type="submit">Add Video</button>
+      </form>
+      {/* New Viedos added printing to the screen */}
+      {
+        videos.map((video) => {
+          const { id, title, url, rating } = video;
+          return (
+            <div key={id}>
+              <h4>{title}</h4>
+              <p>{url}</p>
+              <p>{rating}</p>
+            </div>
+          )
+        })
+      }
     </div>
   )
 }
 
-export default AddVideo
+export default AddVideo;
