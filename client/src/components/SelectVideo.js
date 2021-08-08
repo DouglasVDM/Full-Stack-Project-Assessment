a0b0a6a5e365b3ed4e96001f705d1e4c18eea46c
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-08-08 15:28:31 +0200

    pass url to load selected video

--- client/src/components/SelectVideo.js
+++ client/src/components/SelectVideo.js
@@ -1,35 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Video from './Video';
 
 function SelectVideo({ state }) {
   const allVideos = state;
+  const [url, setUrl] = useState(null);
 
-  const sortedVideos = allVideos.sort((video1, video2) => {
-    let title1 = video1.title.toUpperCase();
-    let title2 = video2.title.toUpperCase();
-    if (title1 < title2) {
-      return -1;
-    } else if (title1 > title2) {
-      return 1;
-    }
-    return 0;
-  })
-  
-  return (
+  const clickHandler = (event) => {
+    const result = event.target.value;
+    return setUrl(result);
+  };
+
+   return (
     <div>
-      <p>Found {sortedVideos.length} videos.</p>
-        <select className="custom-select">
-          <option selected>Choose your video</option>
-        {sortedVideos.map(video => {
+      <p>Found {allVideos.length} videos. Choose one.</p>
+       <select onClick={clickHandler} className="custom-select">
+         <option value='0' >Choose a video</option>
+         {allVideos.map(video => {
           return (
             <>
-              <option key={video.id} value={video.id}>{video.title}</option>
+              <option value={video.url} id={video.id} key={video.id}>
+                {video.title}
+              </option>
             </>
-          )}
-      )}
+           )
+         })}
       </select>
-      <Video state={state} />
-
+       {url ? <Video url={url} state={state} /> : null}
     </div>
   )
 }
