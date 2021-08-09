a6c26bfc5e99b24d66318f043d4e9eea99e31516
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-08-09 17:25:04 +0200

    post new video

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const _ = require('lodash');
 
-
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
@@ -33,11 +34,11 @@ app.post("/", (req, res) => {
     title: title,
     url: url
   }
+
   // Checking if any property of the Video object is missing or empty.
   if (!newVideo.title) {
     res.status(400).json({ msg: `Please include a title` });
-  }
-  if (!newVideo.url) {
+  } if (!newVideo.url) {
     res.status(400).json({ msg: `Please include a valid url` });
   } else {
     videos.push(newVideo);
