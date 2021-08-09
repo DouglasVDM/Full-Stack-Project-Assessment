525154d0265d25451b57f90dd6e0bd259a806e3a
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-08-09 15:45:15 +0200

    server started
    GET all videos from main root /
    Imported JSON file.

--- server/server.js
+++ server/server.js
@@ -1,15 +1,27 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
 
 // Store and retrieve your videos from here
 // If you want, you can copy "exampleresponse.json" into here to have some data to work with
-let videos = [];
+let videos = require('./../exampleresponse.json');
 
 // GET "/"
 app.get("/", (req, res) => {
-  // Delete this line after you've confirmed your server is running
-  res.send({ express: "Your Backend Service is Running" });
+  res.send(videos);
+});
+
+// POST "/"
+app.post("/", (req, res) => {
+  const { title, url } = req.body;
+  const newVideo = {
+    id: 1,
+    title: { title },
+    url: {url}
+  }
+  res.send(videos);
 });
