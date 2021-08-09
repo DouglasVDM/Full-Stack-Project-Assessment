fb5b737d306720cbb4217abcb818437f91bec9b1
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-08-09 18:46:29 +0200

    install and use cors

--- client/src/components/AllVideos.js
+++ client/src/components/AllVideos.js
@@ -1,12 +1,20 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ExampleResponse from '../assets/exampleResponse.json';
 import AddVideo from './AddVideo';
 import SelectVideo from './SelectVideo';
 
-
 function EmbedVideo() {  
   const data = ExampleResponse;
   const [state, setState] = useState(data)
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setState(data)
+      });
+  }, [])
 
   
   return (
--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const _ = require('lodash');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
