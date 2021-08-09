1cdbfcfbd135fd942f043c6aaaca53f3ce3c1d57
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-08-09 16:58:16 +0200

    add lodash

--- server/server.js
+++ server/server.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const _ = require('lodash');
+
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -18,10 +21,26 @@ app.get("/", (req, res) => {
 // POST "/"
 app.post("/", (req, res) => {
   const { title, url } = req.body;
+
+  // ID for New Video
+  const orderedIdVideos = _.orderBy(videos, ['id'], ['desc']); // 'asc'
+  const latestIdVideo = orderedIdVideos[0];
+  // Just need to check if order videos has a item in the array
+  const newId = latestIdVideo.id + 1;
+
   const newVideo = {
-    id: 1,
-    title: { title },
-    url: {url}
+    id: newId,
+    title: title,
+    url: url
   }
-  res.send(videos);
+  // Checking if any property of the Video object is missing or empty.
+  if (!newVideo.title) {
+    res.status(400).json({ msg: `Please include a title` });
+  }
+  if (!newVideo.url) {
+    res.status(400).json({ msg: `Please include a valid url` });
+  } else {
+    videos.push(newVideo);
+    res.json(videos);
+  }
 });
