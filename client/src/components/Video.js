3da0cbb35144ea1e8403df1203b3ad287dc502b4
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-08-14 21:43:43 +0200

    connet rating to vote

--- client/src/components/Video.js
+++ client/src/components/Video.js
@@ -2,55 +2,66 @@ import React, { useState } from 'react'
 import DownVoteButton from './DownVoteButton';
 import UpVoteButton from './UpVoteButton';
 
-function SelectVideo({ videos }) {
-  const allVideos = videos;
-  const [url, setUrl] = useState(null);
-  const [rating, setRating] = useState(0);
+function SelectVideo({videoId, videoRating, videoUrl, videoTitle }) {
+  // const allVideos = videos;
+  let [rating, setRating] = useState(videoRating);
   
-  const filteredVideo = allVideos.filter(video => video.url === url);
+  // const filteredVideo = allVideos.filter(video => video.url === url);
 
-  const upVote = (setRating, rating) => {
-    return () => setRating(rating + 1);
-  }
+  const vidId = videoUrl.split('=').pop();
 
-  const downVote = (setRating, rating) => {
-    return () => setRating(rating - 1);
-  }
-
-
-  const clickHandler = (event) => {
-    const result = event.target.value;
-    return setUrl(result);
-  };  
-
-  return (   
-    <div>
-       <p>Found {allVideos.length} videos. Choose one.</p>
-      {/* Displaying all the Videos */}
-      {allVideos.map(video => {
-        const videoId = video.url.split('=').pop();
-        return (
-          <div className="videos" style={
-            {
-              background: "lightgreen",
-              border: ".1rem solid blue",
-              margin: ".3rem"              
-            }}>
-            <br />
-            <h6>Title: {video.title}</h6>
-            <div className="container">
-              <iframe className="responsive-iframe" width="560" height="315" src={`https://www.youtube.com/embed/${videoId}`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-            </div>
-            <h6>Rating: {video.rating}</h6>
-            <UpVoteButton rating={video.rating} setRating={setRating} upVote={upVote} />
-            <DownVoteButton rating={rating} setRating={setRating} downVote={downVote} />
-            <br />
-          </div>
-        )
+  const upVote = (video) => {
+    console.log(video);
+    if (video === videoRating) {
+      setRating((video) => {
+        return video + 1
       })
-      }
-    </div>
+    } else if (video === videoRating + 1){
+      setRating((video))
+    } else {
+      return
+    }
+  };
+
+  const downVote = (video) => {
+    console.log(video);
+    if (video === videoRating +1) {
+      setRating((video) => {
+        return video - 1
+      })
+    } else if (video === videoRating - 1){
+      setRating((video))
+    } else {
+      return
+    }
+  };
+
+  const deleteVideo = () => {
+    console.log(`Delete me!`);
+  };
+
+
+  return (         
+    <div key={videoId} className="videos" style={
+      {
+        background: "lightgreen",
+        border: ".1rem solid blue",
+        margin: ".3rem"              
+      }}>
+      <h6>Title: {videoTitle}</h6>
+      <br />
+      <div className="container">
+        <iframe className="responsive-iframe" width="560" height="315" src={`https://www.youtube.com/embed/${vidId}`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+      </div>
+      <button onClick={() => upVote(rating)}>Up Vote</button>
+      <h6>Rating: {rating}</h6>
+      <button onClick={() => downVote(rating)}>Down Vote</button>
+      <br />
+      <div>
+        <button onClick={deleteVideo}>Delete</button>
+      </div>
+    </div>       
   )
 }
 
-export default SelectVideo
+export default SelectVideo;
